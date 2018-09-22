00f4fa1095ca95f00ec18fe7a026367198b32d82
Kishlin <xd3Rk703pThd9>
2018-09-22 19:40:44 +0200

    Overwritten User entity.

---
+++ src/CoreBundle/Entity/User.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * User: Pierre-Louis Legrand <hello@pierrelouislegrand.fr>
+ * Date: 9/22/2018
+ * Time: 3:42 PM
+ */
+
+namespace CoreBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="vs_user")
+ */
+class User extends BaseUser
+{
+
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+}

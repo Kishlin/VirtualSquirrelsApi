12fe84c97c2c36653429309fdf767d073a9008fa
Kishlin <pierrelouis.legrand@playrion.com>
2018-09-27 16:58:06 +0200

    Bugfix inheritance.

--- src/UserBundle/Fixtures/ORM/UserFixtures.php
+++ src/UserBundle/Fixtures/ORM/UserFixtures.php
@@ -8,13 +8,14 @@
 namespace UserBundle\Fixtures\ORM;
 
 
+use Doctrine\Bundle\FixturesBundle\Fixture;
 use FOS\UserBundle\Util\PasswordUpdaterInterface;
 
 /**
  * @package UserBundle\Fixtures\ORM
  * @author  Pierre-Louis Legrand <pierrelouis.legrand@playrion.com>
  */
-abstract class UserFixtures
+abstract class UserFixtures extends Fixture
 {
 
     /** @var PasswordUpdaterInterface */

b59d583af746a3a7eea504ecc30334fb030a51e1
Kishlin <pierrelouis.legrand@playrion.com>
2018-10-01 11:26:25 +0200

    Core events file.

---
+++ src/CoreBundle/CoreEvents.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * User: Pierre-Louis Legrand <pierrelouis.legrand@playrion.com>
+ * Date: 01/10/2018
+ * Time: 11:25
+ */
+
+namespace CoreBundle;
+
+
+/**
+ * @package CoreBundle
+ * @author  Pierre-Louis Legrand <pierrelouis.legrand@playrion.com>
+ */
+final class CoreEvents
+{
+
+    private function __construct() { }
+
+}

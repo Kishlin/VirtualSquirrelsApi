2d5dc5b80c0275d1bf3fbe1d1232b6622b2e3265
Kishlin <pierrelouis.legrand@playrion.com>
2018-10-01 10:48:07 +0200

    Simplified inheritance.

--- src/UserBundle/Tests/Functional/Util/WebTestCase.php
+++ src/UserBundle/Tests/Functional/Util/WebTestCase.php
@@ -8,48 +8,10 @@
 namespace UserBundle\Tests\Functional\Util;
 
 
-use Doctrine\Common\Persistence\ObjectManager;
-use Doctrine\Common\Persistence\ObjectRepository;
-use Liip\FunctionalTestBundle\Test\WebTestCase as BaseClass;
-use Symfony\Bundle\FrameworkBundle\Client;
+use CoreBundle\Tests\Functional\Util\WebTestCase as BaseClass;
 
 /**
  * @package UserBundle\Tests\Functional\Util
  * @author  Pierre-Louis Legrand <pierrelouis.legrand@playrion.com>
  */
-class WebTestCase extends BaseClass
-{
-
-    /** @var ObjectManager */
-    protected $objectManager;
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $this->objectManager = $this->getContainer()->get('doctrine');
-    }
-
-
-    /**
-     * @param Client $client
-     * @throws \Exception
-     */
-    public function assertJsonResponse(Client $client): void
-    {
-        $this->assertEquals('application/json', $client->getResponse()->headers->get('content-type'));
-    }
-
-    /**
-     * @param string $class
-     * @return ObjectRepository
-     */
-    public function getRepository(string $class): ObjectRepository
-    {
-        return $this->objectManager->getRepository($class);
-    }
-
-}
+class WebTestCase extends BaseClass { }

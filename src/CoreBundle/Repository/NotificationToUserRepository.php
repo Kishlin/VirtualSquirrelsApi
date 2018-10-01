d7463a02c12bfc561b75ef05ed2d21764e21f6b5
Kishlin <pierrelouis.legrand@playrion.com>
2018-10-01 18:06:40 +0200

    Notification entities repositories.

---
+++ src/CoreBundle/Repository/NotificationToUserRepository.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * User: Pierre-Louis Legrand <hello@pierrelouislegrand.fr>
+ * Date: 10/01/2018
+ * Time: 5:43 PM
+ */
+
+namespace CoreBundle\Repository;
+
+
+/**
+ * @package CoreBundle\Repository
+ * @author  Pierre-Louis Legrand <pierrelouis.legrand@playrion.com>
+ */
+class NotificationToUserRepository extends \Doctrine\ORM\EntityRepository
+{
+}

547a26ab3a91cd77db62375c17b4b226bcb0504f
Kishlin <pierrelouis.legrand@playrion.com>
2018-10-03 12:37:24 +0200

    Find by type method.

--- src/CoreBundle/Repository/NotificationTypeRepository.php
+++ src/CoreBundle/Repository/NotificationTypeRepository.php
@@ -8,10 +8,29 @@
 namespace CoreBundle\Repository;
 
 
+use CoreBundle\Entity\NotificationType;
+
 /**
  * @package CoreBundle\Repository
  * @author  Pierre-Louis Legrand <pierrelouis.legrand@playrion.com>
  */
 class NotificationTypeRepository extends \Doctrine\ORM\EntityRepository
 {
+
+    /**
+     * @param string $type
+     * @return NotificationType|null
+     * @throws \Doctrine\ORM\NoResultException
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public function findOneByType(string $type): ?NotificationType
+    {
+        return $this->createQueryBuilder('notificationType')
+            ->where('notificationType.type = :type')
+            ->setParameter('type', $type)
+            ->getQuery()
+            ->getSingleResult()
+        ;
+    }
+
 }

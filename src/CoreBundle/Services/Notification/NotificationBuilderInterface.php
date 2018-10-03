6f742f9cda7a750ae3f957f69abf26142048d713
Kishlin <pierrelouis.legrand@playrion.com>
2018-10-03 12:48:37 +0200

    Notification builder.

---
+++ src/CoreBundle/Services/Notification/NotificationBuilderInterface.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * User: Pierre-Louis Legrand <pierrelouis.legrand@playrion.com>
+ * Date: 03/10/2018
+ * Time: 12:39
+ */
+
+namespace CoreBundle\Services\Notification;
+
+
+use CoreBundle\Entity\Notification;
+use CoreBundle\Model\NotificationTarget;
+
+
+/**
+ * @package CoreBundle\Services\Notification
+ * @author  Pierre-Louis Legrand <pierrelouis.legrand@playrion.com>
+ */
+interface NotificationBuilderInterface
+{
+
+    /**
+     * @param NotificationTarget|null $target
+     * @param string                  $type
+     * @param \DateTime               $dateTime
+     * @param string                  $subject
+     * @param string                  $message
+     * @return Notification
+     */
+    function build(?NotificationTarget $target,
+                   string $type,
+                   \DateTime $dateTime,
+                   string $subject,
+                   string $message): Notification;
+
+}

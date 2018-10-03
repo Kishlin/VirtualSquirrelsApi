6f2844aca2a7da98b7f4b522ab7c59756fcb3ab2
Kishlin <pierrelouis.legrand@playrion.com>
2018-10-03 12:37:04 +0200

    Runtime exception code.

---
+++ src/CoreBundle/CoreException.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * User: Pierre-Louis Legrand <pierrelouis.legrand@playrion.com>
+ * Date: 03/10/2018
+ * Time: 12:36
+ */
+
+namespace CoreBundle;
+
+
+/**
+ * @package CoreBundle
+ * @author  Pierre-Louis Legrand <pierrelouis.legrand@playrion.com>
+ */
+final class CoreException
+{
+
+    private function __construct() { }
+
+    /** @var int */
+    const RUNTIME_EXCEPTION = 666;
+
+}
--- src/CoreBundle/Exception/RuntimeException.php
+++ src/CoreBundle/Exception/RuntimeException.php
@@ -8,6 +8,8 @@
 namespace CoreBundle\Exception;
 
 
+use CoreBundle\CoreException;
+
 /**
  * @package CoreBundle\Exception
  * @author  Pierre-Louis Legrand <pierrelouis.legrand@playrion.com>
@@ -16,11 +18,12 @@ class RuntimeException extends \RuntimeException
 {
 
     /**
+     * @param string     $message
      * @param \Throwable $previous
      */
-    public function __construct(\Throwable $previous)
+    public function __construct($message, \Throwable $previous)
     {
-        parent::__construct('', 0, $previous);
+        parent::__construct($message, CoreException::RUNTIME_EXCEPTION, $previous);
     }
 
 }

9799a84ee7275449520de0956ff750e6626e8b6d
Kishlin <pierrelouis.legrand@playrion.com>
2018-10-03 12:30:53 +0200

    Enumeration for notification types.

---
+++ src/CoreBundle/Enum/NotificationTypeEnum.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * User: Pierre-Louis Legrand <pierrelouis.legrand@playrion.com>
+ * Date: 03/10/2018
+ * Time: 12:14
+ */
+
+namespace CoreBundle\Enum;
+
+
+/**
+ * @package CoreBundle\Enum
+ * @author  Pierre-Louis Legrand <pierrelouis.legrand@playrion.com>
+ */
+final class NotificationTypeEnum
+{
+
+    private function __construct() { }
+
+}

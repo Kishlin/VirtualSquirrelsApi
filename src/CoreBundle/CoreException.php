42b0c28ce8b20182971f7ac6b167e387a636422b
Kishlin <pierrelouis.legrand@playrion.com>
2018-10-04 18:28:10 +0200

    Common exception with code and message.

--- src/CoreBundle/CoreException.php
+++ src/CoreBundle/CoreException.php
@@ -23,4 +23,11 @@ final class CoreException
     /** @var int */
     const INVALID_ARGUMENT_EXCEPTION = 100;
 
+    /** @var int  */
+    const BAD_REQUEST_EXCEPTION = 200;
+
+
+    /** @var string */
+    const MESSAGE_USER_NOT_FOUND = 'User was not found.';
+
 }

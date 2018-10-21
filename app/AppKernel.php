e7932eda320fab84e1f7a01ae7852cbea546b579
Kishlin <legrand.pierrelouis@gmail.com>
2018-10-21 04:12:53 +0200

    Updated to symfony 4.1.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -26,14 +26,12 @@ class AppKernel extends Kernel
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
 
             if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
                 $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
             }
 
             if ('dev' === $this->getEnvironment()) {
-                $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
                 $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
             }
 
